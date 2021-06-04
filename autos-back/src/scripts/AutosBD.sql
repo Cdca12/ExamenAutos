0dd86c9d94b01015fbbf997371191f5b93ffcbc1
Carlos Contreras <cdca12mx@gmail.com>
2021-06-04 13:36:33 -0600

    Se crea script de la base de datos en MySQL

---
+++ autos-back/src/scripts/AutosBD.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Autos;
+
+USE Autos;
+
+CREATE TABLE Autos (
+	AutoID INT NOT NULL auto_increment,
+    Modelo varchar(50) NOT NULL,
+    Marca varchar(60) NOT NULL,
+    Año varchar(4) NOT NULL,
+    Color varchar(20),
+    PRIMARY KEY (AutoID)
+);
+
+INSERT INTO Autos (Modelo, Marca, Año, Color)
+VALUES 
+	('R8', 'Audi', '2008', 'Negro'), 
+    ('Mustang', 'Ford', '2010', 'Rojo');
+    
+/* TEST */
+SELECT * FROM Autos
